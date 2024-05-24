5a33624a88003a4dd3c0b1ec459880836be5b689
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-05-24 13:13:54 +0700

    Add: title for navbar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const THIS_YEAR = new Date().getFullYear();
 
   return (
-    <footer className="h-fit">
+    <footer className="h-fit p-4">
       <p>&copy; {THIS_YEAR} Le Dinh Khoi</p>
     </footer>
   );
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,8 @@
 export default function Nav() {
   return (
-    <nav className="fixed bg-slate-400 w-full">
-      <ul className="flex gap-4 p-4">
+    <nav className="fixed bg-slate-400 w-full flex justify-between items-center p-4">
+      <h1>LeKo</h1>
+      <ul className="flex gap-4">
         <li>
           <a href="/">Home</a>
         </li>
