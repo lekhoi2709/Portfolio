8617541a9ba198025e6f8babaaaa36fb1c2e0823
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-05-27 20:40:41 +0700

    Make: adjust footer UI

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const THIS_YEAR = new Date().getFullYear();
 
   return (
-    <footer className="h-fit p-4 font-custom">
+    <footer className="h-fit p-4 font-custom flex items-center justify-center">
       <p>&copy; {THIS_YEAR} Le Dinh Khoi</p>
     </footer>
   );
