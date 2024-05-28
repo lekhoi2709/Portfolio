874db1917c7cac9e401b5c9d1854a9a1c0503ad7
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-05-28 10:45:53 +0700

    Add: give credit to Brittany Chiang

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,17 @@ export default function Footer() {
   const THIS_YEAR = new Date().getFullYear();
 
   return (
-    <footer className="h-fit p-4 font-custom flex items-center justify-center">
-      <p className="text-xs text-slate-500">
-        {THIS_YEAR} Built by Le Dinh Khoi
+    <footer className="h-fit p-4 font-custom flex items-center justify-center text-xs text-slate-500">
+      <p>
+        {THIS_YEAR} Inspired by{" "}
+        <a
+          className="hover:underline cursor-pointer hover:text-blue-500"
+          href="https://v4.brittanychiang.com"
+          target="_blank"
+        >
+          Brittany Chiang
+        </a>
+        . Built by Le Dinh Khoi
       </p>
     </footer>
   );
