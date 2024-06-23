be6fcf858b059f6f26d3838310f721c7f9740ab0
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-06-23 13:12:59 +0700

    fix: remove web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -13,7 +11,4 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
